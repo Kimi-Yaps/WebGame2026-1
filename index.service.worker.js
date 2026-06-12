3f809b491cbffee26ec9a88bc36d3c3a5cd39c97
Kimi-Yaps <168716465+Kimi-Yaps@users.noreply.github.com>
2026-06-12 14:06:50 +0800

    Please provide the changes or the files you would like me to summarize.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781242592|716610268';
+const CACHE_VERSION = '1781244399|2523301806';
 /** @type {string} */
 const CACHE_PREFIX = 'ProceduralMap-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
